5244a9b8287bf2bb18ef10c49b579f1106c30a74
Khalil Kadri <kadrikhalil@gmail.com>
2021-07-19 23:21:55 +0530

    Removed unnecessary code from index.php

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-use Sigfw\Service\Config;
-use Sigfw\Service\Connection;
-use Sigfw\Service\Logger;
 use Sigfw\Service\Model;
 use Sigfw\Service\Utils;
 
