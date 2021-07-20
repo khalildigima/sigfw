08b76b2137ecca003b6e00477ce3af74ea5eb21a
Khalil Kadri <kadrikhalil@gmail.com>
2021-07-20 06:13:15 +0530

    get_custom() function in Posts.php which extends Model

--- src/Model/Posts.php
+++ src/Model/Posts.php
@@ -3,11 +3,36 @@
 namespace Sigfw\Model;
 
 use Sigfw\Service\Model;
+use Sigfw\Service\Logger;
+use PDO;
+use PDOException;
 
 class Posts extends Model
 {
     public function __construct()
     {
         parent::__construct("posts");
+    }
+
+    public function get_custom()
+    {
+        $con = $this->verification_connection();
+        if(!$con) return null;
+        
+        $sql = "SELECT * FROM `{$this->get_table_name()}` where `id` > 18;";
+
+        try
+        {
+            $stmt = $con->prepare($sql);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            return $stmt->fetchAll();
+        }
+        catch(PDOException $e) 
+        {
+            Logger::add_error_message("Failed to get data: " . $e->getMessage());
+            return null;
+        }
     }
 }
