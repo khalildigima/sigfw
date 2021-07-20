41ce35656ab500e02987c0ee1863a4b5e09da172
Khalil Kadri <kadrikhalil@gmail.com>
2021-07-20 05:32:18 +0530

    Configuration will be load from .env file

--- src/Service/Config.php
+++ src/Service/Config.php
@@ -1,6 +1,8 @@
 <?php 
 namespace Sigfw\Service;
 
+use Sigfw\Service\Logger;
+
 class Config
 {
     private $debug = true;
@@ -15,6 +17,65 @@ class Config
     public function set_deploy($value) { $this->deploy = $value; }
     public function is_deploy() { return $this->deploy; }
 
+    public function __construct()
+    {
+        $file = __DIR__ . "/.env";
+        $this->load_configuration_from_file($file);
+    }
+
+    public function load_configuration_from_file($file)
+    {
+        $this->load_env($file);
+        
+        $_development = array(
+            "host" => getenv('DEVELOPMENT_HOSTNAME'),
+            "username" => getenv('DEVELOPMENT_USERNAME'),
+            "password" => getenv('DEVELOPMENT_PASSWORD'),
+            "dbname" => getenv('DEVELOPMENT_DBNAME')
+        );
+
+        $_production = array(
+            "host" => getenv('PRODUCTION_HOSTNAME'),
+            "username" => getenv('PRODUCTION_USERNAME'),
+            "password" => getenv('PRODUCTION_PASSWORD'),
+            "dbname" => getenv('PRODUCTION_DBNAME')
+        );
+
+        $this->debug = getenv('DEBUG');
+        $this->deploy = getenv('DEPLOY');
+
+        $this->set_development_config($_development);
+        $this->set_production_config($_production);
+
+    }
+
+    public function load_env($file)
+    {
+        if(!file_exists($file)) 
+        {
+            Logger::add_error_message("Config: ENV file not found");
+            return;
+        }
+
+        $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) 
+        {
+
+            if (strpos(trim($line), '#') === 0) { continue; }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) 
+            {
+                putenv(sprintf('%s=%s', $name, $value));
+                $_ENV[$name] = $value;
+                $_SERVER[$name] = $value;
+            }
+        }
+    }
+    
     private function validate_configuration($config)
     {
         if(!is_array($config)) return false;
