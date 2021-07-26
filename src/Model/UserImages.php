c6130c019ca8c25b50773506407067de50c85646
Khalil Kadri <kadrikhalil@gmail.com>
2021-07-26 12:56:41 +0530

    Model for UserImage

---
+++ src/Model/UserImages.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Sigfw\Model;
+
+use Sigfw\Service\Model;
+use Sigfw\Service\Logger;
+use Sigfw\Model\Users;
+use PDO;
+use PDOException;
+
+class Users extends Model
+{
+    public function __construct()
+    {
+        parent::__construct("users");
+    }
+
+    public function get_images($user_id)
+    {
+        return $this->get_all_by("user_id", $user_id);
+    }
+	
+	public function get_user($user_id)
+	{
+		$model = new Users();
+		return $model->get_by_id($user_id);
+	}
+}
